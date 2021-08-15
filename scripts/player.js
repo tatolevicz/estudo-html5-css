fc9175b0949d50dee83fac105b4c09f2e9108cea
Tato Levicz <tatolevicz@gmail.com>
2021-08-15 17:53:01 -0300

    fix image not rendering

--- scripts/player.js
+++ scripts/player.js
@@ -1,106 +1,100 @@
+class Player {
+  constructor(img, scale) {
+    this.img = img;
+    this.scale = scale;
+    this.rotation = 0;
+    this.rotSpeed = 0.0;
 
-class Player{
+    this.speedY = 0;
+    this.gravity = 0.2;
 
+    this.speed = 1;
 
-    constructor(img, scale){
-        
-        this.img = img;
-        this.scale = scale;
-        this.rotation = 0;
-        this.rotSpeed = 0.0;
+    this.width = this.img.width * this.scale;
+    this.height = this.img.height * this.scale;
 
-        this.speedY = 0;
-        this.gravity = 0.2;
+    this.x = 0;
+    this.y = 0;
 
-        this.speed = 1;
-        
-        this.width = this.img.width*this.scale;
-        this.height = this.img.height*this.scale;
+    this.grounded = false;
+    this.lastGroundedState = false;
 
-        this.x = 0;
-        this.y = 0;
+    this.onGrounded = undefined;
 
-        this.grounded = false;
-        this.lastGroundedState = false;
+    this.img = new Image();
+    this.img.src = "./assets/images/player.png";
+  }
 
-        this.onGrounded = undefined;
+  draw() {
+    var canvas = document.querySelector("canvas");
+    var ctx = canvas.getContext("2d");
 
-        this.img = new Image();
-        this.img.src = './assets/images/player.png';
+    ctx.save();
+    ctx.beginPath();
+    ctx.translate(this.x, this.y - this.height / 2);
+    ctx.rotate(this.rotation);
+
+    ctx.translate(0, this.height / 2);
+    ctx.drawImage(
+      this.img,
+      -this.width / 2,
+      -this.height,
+      this.width,
+      this.height
+    );
+
+    ctx.restore();
+  }
+
+  setScale(scale) {
+    this.scale = scale;
+    this.width = this.img.width * this.scale;
+    this.height = this.img.width * this.scale;
+  }
+
+  setPosition(x, y) {
+    if (this.y < y - 10) {
+      this.grounded = false;
+
+      this.rotation += 0.01;
+    } else {
+      this.grounded = true;
     }
 
-    draw(){
-
-        var canvas = document.querySelector("canvas");
-        var ctx = canvas.getContext("2d");        
-
-        ctx.save();
-
-        ctx.translate(this.x,this.y - this.height/2);
-        ctx.rotate(this.rotation);
-
-        ctx.translate(0,this.height/2);
-        ctx.drawImage(this.img,-this.width/2, -this.height, this.width ,this.height);
-
-        ctx.restore();
+    //just change the state
+    if (this.lastGroundedState != this.grounded) {
+      // console.log("Grounded: " + this.grounded);
+      if (this.grounded && this.onGrounded) this.onGrounded();
     }
 
-    setScale(scale){
-        this.scale = scale;
-        this.width = this.img.width*this.scale;
-        this.height = this.img.width*this.scale;
+    this.lastGroundedState = this.grounded;
+
+    if (this.y < y) {
+      this.speedY += this.gravity;
+    } else {
+      this.speedY -= (this.y - y) * this.speed * this.gravity;
+      this.y = y;
     }
 
-    setPosition(x,y){
+    this.x = x;
+    // console.log(this.speedY);
+    this.y += this.speedY;
+  }
 
-        if(this.y < (y - 10))
-        {
-            this.grounded = false;
+  rotate(increment) {
+    this.rotation -= increment * this.rotSpeed;
 
-            this.rotation += 0.01;
-        }
-        else{
-            this.grounded = true;
-        }
+    if (this.rotation > Math.PI) this.rotation = -Math.PI;
+    if (this.rotation < -Math.PI) this.rotation = Math.PI;
+  }
 
-         //just change the state
-        if(this.lastGroundedState != this.grounded)
-        {
-            // console.log("Grounded: " + this.grounded);
-            if(this.grounded && this.onGrounded)
-                this.onGrounded();
-        }
-    
-        this.lastGroundedState  = this.grounded;
+  setSpeed(speed) {
+    this.speed = Math.abs(speed);
+  }
 
-        if(this.y < y)
-        {
-            this.speedY += this.gravity;
-        }
-        else{
-            this.speedY -= (this.y - y) * this.speed*this.gravity;
-            this.y = y;
-        }
-        
-        this.x = x;
-        // console.log(this.speedY);
-        this.y += this.speedY;
-    }
-
-    rotate(increment){
-        this.rotation -= increment*this.rotSpeed;
-
-        if(this.rotation > Math.PI) this.rotation = -Math.PI;
-        if(this.rotation < -Math.PI) this.rotation = Math.PI;
-    }
-
-    setSpeed(speed){
-        this.speed = Math.abs(speed);
-    }
-
-    isGrounded(){
-        return this.grounded;
-    }
+  isGrounded() {
+    return this.grounded;
+  }
 }
 
-export {Player};
+export { Player };
